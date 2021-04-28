a5fe2f02a80f51d4dd0ef7f05bee86169728b86d
Malik5547 <73483688+Malik5547@users.noreply.github.com>
2021-04-28 08:57:50 +0300

    created notebook and smartphone product models

--- store/models.py
+++ store/models.py
@@ -30,6 +30,35 @@ class Product(models.Model):
         return self.title
 
 
+class NotebookProduct(Product):
+
+    diagonal = models.CharField(max_length=255, verbose_name='Diagonal')
+    display_type = models.CharField(max_length=255, verbose_name='Display type')
+    cpu_freq = models.CharField(max_length=255, verbose_name='CPU Frequency')
+    ram = models.CharField(max_length=255, verbose_name='RAM')
+    video = models.CharField(max_length=255, verbose_name='Video card')
+    time_without_charge = models.CharField(max_length=255, verbose_name='Time without charging')
+
+    def __str__(self):
+        return '{} : {}'.format(self.category, self.title)
+
+
+class Smartphone(Product):
+
+    diagonal = models.CharField(max_length=255, verbose_name='Diagonal')
+    display_type = models.CharField(max_length=255, verbose_name='Display type')
+    resolution = models.CharField(max_length=255, verbose_name='Display resolution')
+    accum_value = models.CharField(max_length=255, verbose_name='Accumulator value')
+    ram = models.CharField(max_length=255, verbose_name='RAM')
+    sd = models.BooleanField(default=True)
+    sd_volume_max = models.CharField(max_length=255, verbose_name='SD maximal volume')
+    main_cam_mp = models.CharField(max_length=255, verbose_name='Main camera')
+    frontal_cam_mp = models.CharField(max_length=255, verbose_name='Frontal camera')
+
+    def __str__(self):
+        return '{} : {}'.format(self.category, self.title)
+
+
 class CartProduct(models.Model):
 
     user = models.ForeignKey('Customer', verbose_name='Customer', on_delete=models.CASCADE)
