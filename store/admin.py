5033878b2a0ce52e42e07e81a8160ac83de871c2
Malik5547 <73483688+Malik5547@users.noreply.github.com>
2021-04-30 20:51:29 +0300

    added image minimal size

--- store/admin.py
+++ store/admin.py
@@ -1,11 +1,34 @@
+from PIL import Image
+
 from django import forms
-from django.forms import ModelChoiceField
+from django.forms import ModelChoiceField, ModelForm, ValidationError
 from django.contrib import admin
 
 from .models import *
 
 
+class NotebookAdminForm(ModelForm):
+
+    MIN_RESOLUTION = 400
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['image'].help_text = 'Upload only image with minimax resolution {}x{}'.format(
+            self.MIN_RESOLUTION, self.MIN_RESOLUTION
+        )
+
+    def clean_image(self):
+        image = self.cleaned_data['image']
+        img = Image.open(image)
+        min_height, min_width = self.MIN_RESOLUTION
+        if img.height < min_height or img.width < min_width:
+            ValidationError('Image resolution is smaller than minimal')
+        return image
+
+
 class NotebookAdmin(admin.ModelAdmin):
+
+    form = NotebookAdminForm
 
     def formfield_for_foreignkey(self, db_field, request, **kwargs):
         if db_field.name == 'category':
