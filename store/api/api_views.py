76b34d013085f8e971cdc7d83708bcdcd422fc29
Malik5547 <73483688+Malik5547@users.noreply.github.com>
2021-05-07 11:17:02 +0300

    api for smartphones with searchfilter

--- store/api/api_views.py
+++ store/api/api_views.py
@@ -1,10 +1,20 @@
 from rest_framework.generics import ListAPIView
+from rest_framework.filters import SearchFilter
 
-from .serializers import CategorySerializer
-from ..models import Category
+from .serializers import CategorySerializer, SmartphoneSerializer
+from ..models import Category, Smartphone
 
 
 class CategoryListAPIView(ListAPIView):
 
     serializer_class = CategorySerializer
     queryset = Category.objects.all()
+
+
+class SmartphoneListAPIView(ListAPIView):
+
+    serializer_class = SmartphoneSerializer
+    queryset = Smartphone.objects.all()
+    filter_backends = [SearchFilter]
+    search_fields = ['title', 'price']
+
--- store/api/serializers.py
+++ store/api/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from ..models import Category
+from ..models import Category, Smartphone
 
 
 class CategorySerializer(serializers.ModelSerializer):
@@ -13,3 +13,30 @@ class CategorySerializer(serializers.ModelSerializer):
         fields = [
             'id', 'name', 'slug',
         ]
+
+
+class BaseProductSerializer:
+
+    category = serializers.PrimaryKeyRelatedField(queryset=Category.objects)
+    title = serializers.CharField()
+    slug = serializers.SlugField()
+    image = serializers.ImageField()
+    description = serializers.CharField(required=False)
+    price = serializers.DecimalField(max_digits=9, decimal_places=2)
+
+
+class SmartphoneSerializer(BaseProductSerializer, serializers.ModelSerializer):
+
+    diagonal = serializers.CharField()
+    display_type = serializers.CharField()
+    resolution = serializers.CharField()
+    accum_value = serializers.CharField()
+    ram = serializers.CharField()
+    sd = serializers.BooleanField()
+    sd_volume_max = serializers.CharField(required=False)
+    main_cam_mp = serializers.CharField()
+    frontal_cam_mp = serializers.CharField()
+
+    class Meta:
+        model = Smartphone
+        fields = '__all__'
