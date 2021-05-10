ac1c381720e4b35e9980b950c16174064a8f571e
Malik5547 <73483688+Malik5547@users.noreply.github.com>
2021-05-10 10:43:58 +0300

    registration implemented

--- store/forms.py
+++ store/forms.py
@@ -36,5 +36,51 @@ class LoginForm(forms.ModelForm):
         user = User.objects.filter(username=username).first()
         if user:
             if not user.check_password(password):
-                raise forms.ValidationError('Incorect password.')
+                raise forms.ValidationError('Incorrect password.')
+        return self.cleaned_data
+
+
+class RegistrationForm(forms.ModelForm):
+
+    class Meta:
+        model = User
+        fields = ['email', 'username', 'password', 'confirm_password', 'first_name', 'last_name', 'address', 'phone']
+
+    password = forms.CharField(widget=forms.PasswordInput)
+    confirm_password = forms.CharField(widget=forms.PasswordInput)
+    phone = forms.CharField(required=False)
+    address = forms.CharField(required=False)
+    email = forms.CharField()
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['username'].label = 'Username'
+        self.fields['password'].label = 'Password'
+        self.fields['confirm_password'].label = 'Confirm password'
+        self.fields['phone'].label = 'Phone'
+        self.fields['first_name'].label = 'First name'
+        self.fields['last_name'].label = 'Last name'
+        self.fields['address'].label = 'Address'
+        self.fields['email'].label = 'Email'
+
+    def clean_email(self):
+        email = self.cleaned_data['email']
+        domain = email.split('.')[-1]
+        if domain in ['com', 'net']:
+            raise forms.ValidationError(f'Registration for domain {domain} is not supported.')
+        if User.objects.filter(email=email).exists():
+            raise forms.ValidationError('This email is already used.')
+        return email
+
+    def clean_username(self):
+        username = self.cleaned_data['username']
+        if User.objects.filter(username=username).exists():
+            raise forms.ValidationError(f'Username {username} is used.')
+        return username
+
+    def clean(self):
+        password = self.cleaned_data['password']
+        confirm_password = self.cleaned_data['confirm_password']
+        if password != confirm_password:
+            raise forms.ValidationError('Passwords dont match.')
         return self.cleaned_data
